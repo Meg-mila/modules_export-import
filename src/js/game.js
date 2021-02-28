39b1f4ee8abc5b868890954654d3f26027d7ea87
Margarita Perepecho <meg.mila27@gmail.com>
2021-02-28 19:51:25 +0000

    task import-export done

---
+++ src/js/game.js
@@ -0,0 +1,17 @@
+import Character from './domain.js';
+
+class Game {
+    start() {
+        console.log('game started');
+    }
+}
+export default Game;
+export class GameSavingData {
+}
+
+export function readGameSaving() {
+}
+
+export function writeGameSaving() {
+}
+
